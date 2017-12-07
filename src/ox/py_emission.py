6702cb9ea03852f3cdc84dce58bab38da554382c
Adrianne Alves <driah_as@hotmail.com>
2017-12-07 11:07:53 -0300

    Adding variable and type node

--- src/ox/py_emission.py
+++ src/ox/py_emission.py
@@ -1,6 +1,7 @@
 from sidekick import opt
 from types import SimpleNamespace
 
+#Node types
 Node = (
     opt.Number(1)
     | opt.Boolean(1)
@@ -11,6 +12,8 @@ Node = (
     | opt.Sub(2)
     | opt.FunCall(2)
 
+    | opt.Variable(2)
+    | opt.Type(1)
     | opt.Equal(2)
     | opt.NotEqual(2)
     | opt.GreaterThan(2)
@@ -32,7 +35,7 @@ Node = (
     | opt.ElifBlock(2)
     | opt.ElseBlock(1)
 
-    | opt.FunDef(3)
+    | opt.FunDef(4)
     | opt.FunReturn(1)
     | opt.Arg(2)
     | opt.FunArg(1)
@@ -65,6 +68,10 @@ FunCall = Node.FunCall
 
 SimpleStatement = Node.SimpleStatement
 
+Variable = Node.Variable
+
+Type = Node.Type
+
 ctx = SimpleNamespace(tokens=[], indent=0)
 
 def source(ast):
@@ -92,6 +99,12 @@ class Options:
 
     def boolean(self, condition):
         ctx.tokens.append(condition)
+
+    def variable(self, _type, name):
+        ctx.tokens,append(name)
+
+    def type(self, _type):
+        ctx.tokens.append(_type)
 
     def add(self, first_argument, second_argument):
         ctx.tokens.append('(')
@@ -198,14 +211,12 @@ class Options:
         ctx.indent -= 1
 
     def ifblock(self, condition, block):
-        ctx.tokens.append('    ' * ctx.indent)
         ctx.tokens.append('if ')
         visit(ctx, condition)
         ctx.tokens.append(' :\n')
         ctx.indent += 1
         visit(ctx, block)
         ctx.indent -= 1
-        ctx.tokens.append('    ' * ctx.indent)
 
     def elseblock(self, block):
         ctx.tokens.append('    ' * ctx.indent)
@@ -213,7 +224,6 @@ class Options:
         ctx.indent += 1
         visit(ctx, block)
         ctx.indent -= 1
-        ctx.tokens.append('    ' * ctx.indent)
 
     def elifblock(self, condition, block):
         ctx.tokens.append('    ' * ctx.indent)
@@ -223,7 +233,6 @@ class Options:
         ctx.indent += 1
         visit(ctx, block)
         ctx.indent -= 1
-        ctx.tokens.append('    ' * ctx.indent)
         ctx.tokens.append('\n')
 
     def funarg(self, arguments):
@@ -249,7 +258,7 @@ class Options:
         visit(ctx, arguments)
         context.tokens.append(')')
 
-    def fundef(self, function_name, arguments, block):
+    def fundef(self, type_return, function_name, arguments, block):
         ctx.tokens.append('def ')
         visit(ctx, function_name)
         ctx.tokens.append('(')
@@ -261,7 +270,6 @@ class Options:
         ctx.tokens.append('\n')
 
     def funreturn(self, _return):
-        ctx.indent += 1
         ctx.tokens.append('return')
         visit(ctx, _return)
 
@@ -277,15 +285,18 @@ returnf = SimpleStatement(FunReturn(Add(Number(2), Number(40))))
 expr1 = SimpleStatement(NameAttrib('x', (Add(Name('x'), Number(1)))))
 expr2 = SimpleStatement(NameAttrib('y', Number(42)))
 block2 = Block([expr1, expr2])
-expr3 = WhileBlock(OrOp(AndOp(Equal(Name('x'), Boolean('true')), Equal(Name('y'), Number(10))),\
-    AndOp(Equal(Name('x'), Number(2)), Equal(Name('y'), Number(10)))), block2)
-
+expr10 = SimpleStatement(IfBlock(Equal(Name('n'), Number('1')),block2))
 expr4 = ForBlock(Name('x'), Number(2), block2)
-block = Block([expr1, expr2, expr3, expr4, returnf])
+block3 = Block([expr10, expr4])
+
+expr3 = WhileBlock(OrOp(AndOp(Equal(Name('x'), Boolean('true')), Equal(Name('y'), Number(10))),\
+    AndOp(Equal(Name('x'), Number(2)), Equal(Name('y'), Number(10)))), block3)
+
+
+block = Block([expr3, returnf])
 
 test1 = Arg('int', 't1')
 test2 = Arg('int', 't2')
 
-expr = FunDef(Name('teste'),FunArg([test1,test2]),block)
-
+expr = FunDef(Type(''),Name('teste'),FunArg([test1,test2]),block)
 print(source(expr))
